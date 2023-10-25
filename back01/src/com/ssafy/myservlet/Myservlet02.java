9d7413e26ce134fea51a1ed5f2799b47dae710f2
정지헌 <hun01414@naver.com>
2023-10-25 10:19:45 +0900

    123

---
+++ back01/src/com/ssafy/myservlet/Myservlet02.java
@@ -0,0 +1,17 @@
+package com.ssafy.myservlet;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Myservlet02 extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+}
